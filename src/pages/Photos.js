4cab515d0165d161e54d5597ac923f04f8201c50
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-13 12:05:59 -0400

    Now displays photos on home page

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,29 @@
 import React from "react"
+import { Context } from "../Context"
+import Image from "../components/Image"
 
 export default function Photos(){
+    function getSizeClass(id){
+        if (id % 5 === 0)
+            return "big"
+        else if (id % 6 === 0)
+            return "wide"
+        else
+            return ""
+    }
+
+    const {allPhotos} = React.useContext(Context)
+    const imageElements = allPhotos.map((item, index) => (
+        <Image
+            key={item.id}
+            img={item}
+            className={getSizeClass(index)}
+        />
+    ))
+
     return (
-        <h1>Home Page</h1>
+        <main className="photos">    
+            {imageElements}
+        </main>
     )
 }
