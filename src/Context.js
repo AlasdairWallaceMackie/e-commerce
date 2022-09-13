8a11909a7cd55f6744a42cf904ec32e9c64c9a77
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-13 12:05:08 -0400

    Add API call

--- src/Context.js
+++ src/Context.js
@@ -3,8 +3,25 @@ import React from "react"
 const Context = React.createContext()
 
 function ContextProvider(props){
+    const [allPhotos, setAllPhotos] = React.useState([])
+
+    function toggleFavorite(id){
+        setAllPhotos(prevState => prevState.map(photo => (
+            {
+                ...photo,
+                isFavorite: photo.id === id ? !photo.isFavorite : photo.isFavorite
+            }
+        )))
+    }
+
+    React.useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+        }, [])
+
     return (
-        <Context.Provider value={""}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {props.children}
         </Context.Provider>
     )
