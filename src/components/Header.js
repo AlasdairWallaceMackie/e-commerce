d0d6eb94bf62e8d12a11f6231a4aa2b49fcbc818
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-14 12:35:33 -0400

    Cart icon will show # of items in cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react"
 import {Link} from "react-router-dom"
 
+import {Context} from "../Context"
+
 export default function Header(){
+    const {cartItems} = React.useContext(Context)
+
+    const cartIcon = cartItems.length ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+
     return (
         <nav className="d-flex justify-content-between p-3 bg-success text-light shadow">
             <Link to="/" className="nav-link"><h1>E-Commerce Site</h1></Link>
-            <Link to="/cart" className="nav-link"><i className="ri-shopping-cart-line ri-2x"></i></Link>
+            <Link to="/cart" className="nav-link">
+                <i className={`${cartIcon} ri-2x`}></i>
+                <span className="position-absolute top-25 start-75 translate-middle badge rounded-pill bg-warning text-dark">
+                    {cartItems.length}
+                </span>
+            </Link>
         </nav>
     )
 }
